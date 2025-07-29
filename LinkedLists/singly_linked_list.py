de832910bc53bff8a5d9c06969ba667143d6ab08
Rakesh-az <rrn161803@gmail.com>
2025-07-29 11:52:53 +0530

    Singly Linked List basic structure

---
+++ LinkedLists/singly_linked_list.py
@@ -0,0 +1,31 @@
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_head(self,data):
+        new_node = node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_tail(self, data):
+        new_node = node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def traverse(self):
+        result = []
+        current_node = self.head
+        while current_node:
+            result.append(current_node.data)
+            current_node = current_node.next
+        return result
